0605b99d9ad1b9e47cfe54dccdd2648283ce557d
yassine abderrahmani <yassine04.97@hotmail.fr>
2020-01-09 01:20:43 +0100

    Petits changements

---
+++ auto.js
@@ -0,0 +1,20 @@
+// var width = 960,
+//     height = 1160;
+
+// var projection = d3.geo.mercator()
+//     .scale(500)
+//     .translate([width / 2, height / 2]);
+
+// var path = d3.geo.path()
+//     .projection(projection);
+
+// var svg = d3.select("body").append("svg")
+//     .attr("width", width)
+//     .attr("height", height);
+
+// d3.json("uk.json", function(error, uk) {
+//   svg.append("path")
+//       .datum(topojson.feature(uk, uk.objects.subunits))
+//       .attr("d", path);
+// });
+
